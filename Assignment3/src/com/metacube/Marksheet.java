8563b4ce02ffe1ee8c95c396f7785146e01afb9e
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-07-16 19:06:46 +0530

    Add files via upload

--- Assignment3/src/com/metacube/Marksheet.java
+++ Assignment3/src/com/metacube/Marksheet.java
@@ -3,77 +3,44 @@ package com.metacube;
 import java.util.*;
 
 public class Marksheet {
-	private int id;
-	private int grades;
-
-		void setGrades(int marks){
-			this.grades = marks;
-		}
-
-		int getGrades(){
-			return this.grades;
-		}
-
-		void setId(int id){
-			this.id = id;
-		}
-
-		int getId(){
-			return this.id;
-		}
-
-	public static void main(String[] args) {
-		Marksheet marksheet = new Marksheet();
-		List<Marksheet> studentGrade = new ArrayList<Marksheet>();
-		Scanner input = new Scanner(System.in);
-		int numberOfStudents, id, marks;
-
-		System.out.println("Enter the number of Students: ");
-		numberOfStudents = input.nextInt();
-		//To check the inputs are valid
-		for(int loop = 0; loop < numberOfStudents; loop++){
-			System.out.println("Enter student id: ");
-			id = input.nextInt();
-			
-			System.out.println("Enter student marks: ");
-			marks = input.nextInt();
-			
-			if((marks < 0 || marks > 100) || id < 1){
-				System.out.println("Wrong input");
-				--loop;
-				continue;
-			}
-			else{
-				Marksheet obj1 = new Marksheet();
-				obj1.setGrades(marks);
-				obj1.setId(id);
-				studentGrade.add(obj1);
-			}
-			
-			
-		}
-
-		System.out.println("Avg: " + String.format("%.2f",marksheet.average(studentGrade)));
-		System.out.println("MAX: " + marksheet.maxGrade(studentGrade));
-		System.out.println("MIN: " + marksheet.minGrade(studentGrade));
-		System.out.println("Pass: " + marksheet.studentsPassed(studentGrade));
+	public Integer id;
+	public Integer grades;
+	
+		
+	public Integer getId() {
+		return id;
 	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getGrades() {
+		return grades;
+	}
+
+	public void setGrades(Integer grades) {
+		this.grades = grades;
+	}
+
 	/**
 	 * to check average
 	 * @param studentGrade
 	 * @return average
 	 */
-	double average(List<Marksheet> studentGrade){
+	public  double average(List<Marksheet> studentGrade){
 		double totalMarks = 0;
+		double passed = 0;
+		for(Marksheet obj : studentGrade){
+			totalMarks += obj.grades;
+		}
 		try {
-			for(Marksheet obj : studentGrade){
-				totalMarks += obj.grades;
-			}
+			passed = totalMarks/studentGrade.size();
 		}
-		catch (Exception e) {
-			System.out.println("Arithmatic error: wrong input in average");
+		catch (ArithmeticException e) {
+			System.out.println("cannot divide by zero");
 		}
-		return totalMarks/studentGrade.size();
+		return passed;
 	}
 
 	/**
@@ -81,17 +48,13 @@ public class Marksheet {
 	 * @param studentGrade
 	 * @return max
 	 */
-	int maxGrade(List<Marksheet> studentGrade){
+	public int maxGrade(List<Marksheet> studentGrade){
 		int max = 0;
-		try{
-			for (Marksheet obj : studentGrade){
-				if(obj.grades > max){
-					max = obj.grades;
-				}
+
+		for (Marksheet obj : studentGrade){
+			if(obj.grades > max){
+				max = obj.grades;
 			}
-		}
-		catch (Exception e) {
-			System.out.println("Arithmatic error: wrong input in maxGrade");
 		}
 		return max;
 	}
@@ -101,22 +64,14 @@ public class Marksheet {
 	 * @param studentGrade
 	 * @return min
 	 */
-	int minGrade(List<Marksheet> studentGrade){
+	public int minGrade(List<Marksheet> studentGrade){
 		Marksheet obj1 = studentGrade.get(0);
 		int min = obj1.grades;
-		System.out.println("minimum initialize"+min);
-		System.out.println("grade: " + min);
-		try {
-			for (Marksheet obj2 : studentGrade){
-				System.out.println("hello");
-				if(obj2.grades < min){
-					min = obj2.grades;
-					System.out.println("mingrade: " + min);
-				}
+		
+		for (Marksheet obj2 : studentGrade){
+			if(obj2.grades < min){
+				min = obj2.grades;
 			}
-		}
-		catch (Exception e) {
-			System.out.println("Arithmatic error: wrong input in minGrade");
 		}
 		return min;
 	}
@@ -126,17 +81,12 @@ public class Marksheet {
 	 * @param studentGrade
 	 * @return
 	 */
-	double studentsPassed(List<Marksheet> studentGrade){
+	public double studentsPassed(List<Marksheet> studentGrade){
 		double passed = 0;
-		try {
-			for(Marksheet mark : studentGrade){
-				if(mark.grades >= 40){
-					passed += 1;
-				}
+		for(Marksheet mark : studentGrade){
+			if(mark.grades >= 40){
+				passed += 1;
 			}
-		}
-		catch (Exception e) {
-			System.out.println("Arithmatic error: wrong input studentsPassed");
 		}
 		return 100*(passed/studentGrade.size());
 	}
