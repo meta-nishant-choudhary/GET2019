b1ba3d431af9cddd7127e1e3868f2e26a3320369
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-07-17 15:45:47 +0530

    Update Result.java

--- Assignment3/src/com/metacube/Result.java
+++ Assignment3/src/com/metacube/Result.java
@@ -9,7 +9,7 @@ public class Result {
 	public static void main(String[] args) {
 		List<Marksheet> studentGrade = new ArrayList<Marksheet>();
 		Scanner input = new Scanner(System.in);
-		int numberOfStudents = 0, id = 0, marks = 0, flag = 0;
+		int numberOfStudents = 0, marks = 0, flag = 0;
 		
 		do{
 			try	{
@@ -22,66 +22,48 @@ public class Result {
 				flag = 1;
 			}
 			catch (Exception e) {
-				System.out.println("Wrong input. please enter coorect students number");
+				System.out.println("Wrong input. please enter correct students number");
+				input.nextLine();
 			}
 		} while (flag == 0);
 		
 		//To check the inputs are valid
 		flag=0;
 		for(int loop = 0; loop < numberOfStudents; loop++){
-			//check first number is valid
-			do {
-				System.out.println("Enter student id: ");
-				try {
-					id = input.nextInt();
-					flag = 1;
-				}
-				catch (ArithmeticException e) {
-					System.out.println("wrong input");
-					loop--;
-					continue;
-				}
-			} while (flag == 0);
-			
-			if (id < 1) {
-			System.out.println("Wrong input");
-			--loop;
-			continue;
-			}
-			//check the second number is valid
+			//check the mark is valid or not
 			flag=0;
 			do {
 				System.out.println("Enter student marks: ");
 				try {
 					marks = input.nextInt();
 					flag=1;
+					if((marks < 0 || marks > 100)){
+						System.out.println("Wrong input");
+						--loop;
+						System.out.println("wrong input ( marks limit: 0 to 100 )");
+						continue;
+					}
 				}
 				catch (ArithmeticException e) {
-					System.out.println("wrong input");
+					System.out.println("wrong input ( Enter integer digit. )");
 					--loop;
 					continue;
 				}
 			} while (flag == 0);
 			
-			if((marks < 0 || marks > 100) || id < 1){
-				System.out.println("Wrong input");
-				--loop;
-				continue;
-			}
-			else{
-				System.out.println("marks "+marks);
-				Marksheet obj1 = new Marksheet();
-				System.out.println("obj "+obj1);
-				obj1.setGrades(marks);
-				obj1.setId(id);
-				studentGrade.add(obj1);
-			}
+			Marksheet obj1 = new Marksheet();
+			obj1.setGrades(marks);
+			studentGrade.add(obj1);
+			
 		}
-		Marksheet result = new Marksheet();
-		System.out.println("Avg: " + String.format("%.2f", result.average(studentGrade)));
-		System.out.println("MAX: " + result.maxGrade(studentGrade));
-		System.out.println("MIN: " + result.minGrade(studentGrade));
-		System.out.println("Pass: " + result.studentsPassed(studentGrade));
+		
+		System.out.println("Avg: " + String.format("%.2f", Marksheet.average(studentGrade)));
+		System.out.println("MAX: " + Marksheet.maxGrade(studentGrade));
+		System.out.println("MIN: " + Marksheet.minGrade(studentGrade));
+		System.out.println("Pass: " + Marksheet.studentsPassed(studentGrade));
 		}
+	/*public static int inputCheck(  ) {
+		
+	}*/
 	}
 
