77d2c1166145504cac249b86935516e8155d70d6
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-07-17 15:16:15 +0530

    Update HexCalc.java

--- Assignment2/src/com/meatcube/HexCalc.java
+++ Assignment2/src/com/meatcube/HexCalc.java
@@ -5,15 +5,39 @@ import java.lang.Math;
 
 public class HexCalc {
 	
-	public static String hexadecimalNumber = "0123456789ABCDEF";
+	public static final String hexadecimalNumber = "0123456789ABCDEF";
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		String num1 = "";
+		String num2 = "";
+		int flag = 0;
+		do{
+			try {
+				System.out.println("Enter first number: ");
+				num1 = (input.nextLine()).toUpperCase();
+				int no = Integer.parseInt(num1, 16);
+				flag = 1;
+			}
+			catch (Exception e){
+				System.out.println("invalid input. Try Again...");
+				flag = 0;
+			}
+		} while (flag == 0);
 		
-		System.out.println("Enter first number: ");
-		String num1 = (input.nextLine()).toUpperCase();
+		do{
+			try {
+				System.out.println("Enter second number: ");
+				num2 = (input.nextLine()).toUpperCase();
+				int no = Integer.parseInt(num2, 16);
+				flag = 1;
+			}
+			catch (Exception e){
+				System.out.println("invalid input. Try Again...");
+				flag = 0;
+			}
+		} while (flag == 0);
+
 		
-		System.out.println("Enter second number: ");
-		String num2 = (input.nextLine()).toUpperCase();
 		
 		int choice = 0;
 		do
@@ -33,108 +57,93 @@ public class HexCalc {
 			if(choice == 1){
 				System.out.println("Addition : " + add(num1, num2));
 			}
-			else if(choice == 2){
+			else if (choice == 2) {
 				System.out.println("Subtraction : " + subtract(num1, num2));
 			}
-			else if(choice == 3){
+			else if (choice == 3) {
 				System.out.println("Divide : " + divide(num1, num2));
 			}
-			else if(choice == 4){
+			else if (choice == 4) {
 				System.out.println("Multiplication : " + multiplication(num1, num2));
 			}
-			else if(choice == 5){
-				if(equalsTo(num1,num2)){
-					System.out.println("num1 and num2 are equal");
+			else if(choice == 5) {
+				if ( equalsTo(num1,num2) ) {
+					System.out.println(num1 + " and " + num2 + " are equal");
 				}
-				else{
-					System.out.println("num1 and num2 are not equal");
+				else {
+					System.out.println(num1 + " and " + num2 + " are not equal");
 				}
 			}
 			else if(choice == 6){
 				if(greater(num1,num2)){
-					System.out.println("num1 is greater");
+					System.out.println(num1 + " is greater");
 				}
 				else{
-					System.out.println("num2 is greater");
+					System.out.println(num2 + " is greater");
 				}
 			}
 			else if(choice == 7){
-				if(greater(num1,num2)){
-					System.out.println("num2 is less");
+				if(smaller(num1,num2)){
+					System.out.println(num2 + " is less");
 				}
 				else{
-					System.out.println("num1 is less");
+					System.out.println(num1 + " is less");
 				}	
 			}
 			else if (choice == 0){
 				break;
 			}
-		}while(choice!=0);
+		} while(choice != 0);
 		System.out.println("exit");
 	}	
 
 public static String add(String num1, String num2){
-	long res1 = hexaToDecimal(num1); 
-	long res2 = hexaToDecimal(num2);
-	long result = res1 + res2;
-	return decimalToHexa(result);
+	int res1 = hexaToDecimal( num1 ); 
+	int res2 = hexaToDecimal( num2 );
+	int result = res1 + res2;
+	return decimalToHexa( result );
 }
 
 public static String subtract(String num1, String num2){
-	long res1 = hexaToDecimal(num1); 
-	long res2 = hexaToDecimal(num2);
-	long result = Math.abs(res1 - res2);
+	int res1 = hexaToDecimal(num1); 
+	int res2 = hexaToDecimal(num2);
+	int result = res1 - res2;
 	return decimalToHexa(result);
 }
 
 public static String divide(String num1, String num2){
-	long res1 = hexaToDecimal(num1); 
-	long res2 = hexaToDecimal(num2);
-	long result = res1 / res2;
-	return decimalToHexa(result);
+	int result;
+	int res1 = hexaToDecimal( num1 ); 
+	int res2 = hexaToDecimal( num2 );
+	try {
+		result = res1 / res2;
+		return decimalToHexa( result );
+	}
+	catch (ArithmeticException e) {
+		return "Number should not be divided by zero";	
+	}
 }
 
 public static String multiplication(String num1, String num2){
-	long res1 = hexaToDecimal(num1); 
-	long res2 = hexaToDecimal(num2);
-	long result = res1 * res2;
+	int res1 = hexaToDecimal(num1); 
+	int res2 = hexaToDecimal(num2);
+	int result = res1 * res2;
 	return decimalToHexa(result);
 }
 
 //TO COVERT THE HEXADECIMAL NUMBER INTO DECIMAL NUMBER
-public static long hexaToDecimal(String num){
-	long quotient = 0, value = 0, result = 0;
-	
-	for(int loop = num.length()-1; loop >= 0; loop--){
-		char ch = num.charAt(loop);							
-		value = hexadecimalNumber.indexOf(ch);										
-		result += value * pow(16,quotient);			
-		quotient++;		
-		
-	}
-	return result;
+public static int hexaToDecimal(String num){
+	return Integer.parseInt( num, 16 );
 }
 
 //TO CONVERT DECIMAL NO. TO HEXADECIMAL NO.
-public static String decimalToHexa(long decimal){
-	String hexa = "";
-	long remainder;
-	long hexaLimit = 16;
-	
-	do{	
-		remainder = decimal % hexaLimit;
-		char ch = hexadecimalNumber.charAt((int)remainder);
-		decimal = decimal/hexaLimit;
-		hexa = Character.toString(ch)+hexa;
-	}while(decimal>0);
-	
-	return hexa;
+public static String decimalToHexa(int decimal){	
+	return Integer.toHexString( decimal );
 }
 
 //TO FIND THE POWER OF BASE NO. OF POWER power
 public static long pow(long base, long power){
-	long result=1;
-	
+	long result = 1;
 	if(power == 0){
 		return 1;
 	}
@@ -152,60 +161,31 @@ public static long pow(long base, long power){
 
 //check that the numbers are equal or not
 public static boolean equalsTo(String num1,String num2){
-	if(num1.length() == num2.length()){
-		for(int loop = 0; loop < num1.length(); loop++){
-			if(hexadecimalNumber.indexOf(num1.charAt(loop)) == hexadecimalNumber.indexOf(num2.charAt(loop))){
-				continue;
-			}
-			else{
-				return false;
-			}
-		}
+	if (hexaToDecimal( num1 ) == hexaToDecimal( num2 )){
+		return true;
 	}
-	else{
+	else {
 		return false;
 	}
-	return true;
 }
 
 //to check which number is greater
 public static boolean greater(String num1, String num2){
-	if(num1.length() == num2.length()){
-		if(hexaToDecimal(num1) > hexaToDecimal(num2)){
-			return true;
-		}
-
-		/*for(int loop = 0; loop < num1.length(); loop++){
-			if(hexadecimalNumber.indexOf(num1.charAt(loop)) > hexadecimalNumber.indexOf(num2.charAt(loop))){
-				return true;
-			}
-			else if(hexadecimalNumber.indexOf(num1.charAt(loop)) == hexadecimalNumber.indexOf(num2.charAt(loop))){
-				continue;
-			}
-			else{
-				return false;
-			}
-		}
-		return*/ 
-	}
-	else if(num1.length() > num2.length()){
+	if(hexaToDecimal( num1 ) > hexaToDecimal( num2 )){
 		return true;
 	}
-	return false;
+	else {
+		return false;
+	}
 }
 
 //to check th smaller number
 public static boolean smaller(String num1, String num2){
-	if(hexaToDecimal(num1) < hexaToDecimal(num2)){
+	if(hexaToDecimal( num1 ) < hexaToDecimal( num2 )){
 		return true;
 	}
-	
-	else if(num1.length() < num2.length()){
-		return true;
+	else {
+		return false;
 	}
-	return false;
 }
-
-
-
 }
