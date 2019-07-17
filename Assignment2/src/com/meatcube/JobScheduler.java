ac65e1b5b8d660dcaca186d2942c95a586864835
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-07-17 15:08:00 +0530

    Update JobScheduler.java

--- Assignment2/src/com/meatcube/JobScheduler.java
+++ Assignment2/src/com/meatcube/JobScheduler.java
@@ -4,24 +4,36 @@ import java.util.Scanner;
 
 class JobScheduler{
 	public static void main(String[] args) {
-		int [][]FCFS = new int[10][10];
 		Scanner input = new Scanner(System.in);	
-		System.out.println("Enter the number of process: ");
-		int numberOfProcess = input.nextInt();
-		
-		for(int loop = 0; loop < numberOfProcess; loop++){
-			System.out.println("Arrival time: ");
-			FCFS[loop][0] = input.nextInt();
-			
-			System.out.println("Burst time: ");
-			FCFS[loop][1] = input.nextInt();
-		}
+		int [][]FCFS = null;
+		int flag = 0, numberOfProcess = 0;
+		do{
+			try {
+				System.out.println("Enter the number of process: ");
+				numberOfProcess = input.nextInt();
+				FCFS = new int[numberOfProcess][2];
+				
+				for(int loop = 0; loop < numberOfProcess; loop++){
+					System.out.println("Arrival time: ");
+					FCFS[loop][0] = input.nextInt();
+				
+					System.out.println("Burst time: ");
+					FCFS[loop][1] = input.nextInt();
+				}
+				flag = 1;
+			}
+			catch (Exception e){
+				System.out.println("error: invalid input");
+				flag = 0;
+				input.next();
+			}
+		} while(flag == 0);
 		
 		int []CT = completionTime(FCFS);
 		int []TAT = turnAroundTime(CT,FCFS);
 		int []WT = waitingTime(TAT,FCFS);
 		
-		for(int i=0;i<4;i++){
+		for(int i = 0; i < numberOfProcess; i++){
 			System.out.println("Completion time of process " + (i + 1) + ": " + CT[i]);
 			System.out.println("Turn Around Time of process " + (i + 1) + ": " + TAT[i]);
 			System.out.println("Waiting Time of process " + (i + 1) + ": " + WT[i]);
@@ -29,46 +41,27 @@ class JobScheduler{
 		}
 		System.out.println("Average Waiting Time : " + averageWaitingTime(WT));
 
-		int []max = maxWait(WT);
-		System.out.println("longest waiting time for a process " + max[1] + "is : " + max[0]);
-	}
-	public static int[] completionTime(int [][]FCFS){
-		int []CT = new int[10];
-
-		for(int loop1 = 0; loop1 < 4; loop1++){
-			for(int loop2 = 0; loop2 < 2; loop2++){
-				if(FCFS[loop1][loop2] == 0 && CT[0] == 0){
-					loop2 += 1;
-					CT[loop1] = FCFS[loop1][loop2];
-				}
-				else if(FCFS[loop1][loop2] < CT[loop1-1]){
-					loop2 += 1;
-					CT[loop1] = CT[loop1-1] + FCFS[loop1][loop2];
-				}
-				else if(FCFS[loop1][loop2] > CT[loop1]){
-					CT[loop1] = FCFS[loop1][loop2] + FCFS[loop1][loop2+1];
-					loop2 = 1;
-				}
-			}
-		}
-		return CT;
+		int max = maxWait(WT);
+		System.out.println("longest waiting time for a process is " + max);
 	}
 	
 	public static int[] turnAroundTime(int []CT,int [][]FCFS){
-		int[] TAT = new int[4];
+		int size = FCFS.length;
+		int[] TAT = new int[size];
 		int col = 0;
-		for(int row = 0; row < 4; row++){
+		for(int row = 0; row < FCFS.length; row++){
 			TAT[row] = CT[row] - FCFS[row][col];
 		}
 		return TAT;
 	}
 
 	public static int[] waitingTime(int []TAT,int [][]FCFS){
-			int[] WT = new int[4];
-			int col=1;
-			for(int row = 0; row < 4; row++){
-				WT[row] = TAT[row] - FCFS[row][col];
-			}
+		int size = FCFS.length;
+		int[] WT = new int[size];
+		int col=1;
+		for(int row = 0; row < WT.length; row++){
+			WT[row] = TAT[row] - FCFS[row][col];
+		}
 		return WT;
 	}
 
@@ -80,15 +73,31 @@ class JobScheduler{
 		return totalWaitingTime/WT.length;
 	}
 
-	public static int[] maxWait(int []WT){
-		int []max = new int[2]; 
-		max[0] = WT[0];
+	public static int maxWait(int []WT){
+		int max = WT[0];
 		for(int wt = 1; wt < WT.length; wt++){
-			if(max[0] < WT[wt]){
-				max[0] = WT[wt];
-				max[1] = wt;
+			if(max < WT[wt]){
+				max = WT[wt];
 			}
 		}
 		return max;
+	}
+
+	public static int[] completionTime(int [][]FCFS){
+		int []CT = new int[10];
+		int col;
+		for(int row = 0; row < FCFS.length; row++){
+			col = 0;
+			if (FCFS[row][0] >= 0 && CT[0] == 0){
+				CT[row] = FCFS[row][1];
+			}
+			else if (FCFS[row][0] < CT[0]){
+				CT[row] = CT[row-1] + FCFS[row][1];
+			}
+			else if (FCFS[row][0] > CT[0]){
+				CT[row] = FCFS[row][0] + FCFS[row][1];
+			}
+		}
+		return CT;
 	}
 }
