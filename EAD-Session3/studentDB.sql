894b3ebdb244fb2fd6c4bb78616a9782f93a8a57
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-08-30 11:53:54 +0530

    Update studentDB.sql

--- EAD-Session3/studentDB.sql
+++ EAD-Session3/studentDB.sql
@@ -11,14 +11,4 @@ CREATE TABLE student_detail(roll_no int primary key auto_increment,
                             Age INT
 );
 
-show tables;
 
-select * from student_detail;
-update student_detail set 
-first_name ='asdasd', 
-last_name ='choudhary', 
-father_name ='Choudhary', 
-E_mail ='nishant@gmail.com', 
-class =4, 
-Age =4 
-where roll_no =45;
