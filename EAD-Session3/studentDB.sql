c81e5acc39906e334c47cdeda24cfd1d349d6e3f
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-08-30 14:23:33 +0530

    Update studentDB.sql

--- EAD-Session3/studentDB.sql
+++ EAD-Session3/studentDB.sql
@@ -6,7 +6,7 @@ CREATE TABLE student_detail(roll_no int primary key auto_increment,
                             first_name varchar(20), 
                             last_name varchar(20), 
                             father_name varchar(20), 
-                            E_mail varchar(20), 
+                            E_mail varchar(20) unique, 
                             class INT, 
                             Age INT
 );
