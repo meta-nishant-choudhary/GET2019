3b2920d363ff8da6350a95cebed9db91251d6794
meta-nishant-choudhary <52689009+meta-nishant-choudhary@users.noreply.github.com>
2019-09-24 16:30:49 +0530

    Update EmployeeDao.java

--- hibernate1/src/main/java/com/metacube/hibernate1/dao/EmployeeDao.java
+++ hibernate1/src/main/java/com/metacube/hibernate1/dao/EmployeeDao.java
@@ -1,8 +1,10 @@
 package com.metacube.hibernate1.dao;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +14,8 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.orm.hibernate.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
-
-
-
-
-
-
-
 import com.metacube.hibernate1.entities.Employee;
-import com.metacube.hibernate1.fascade.FriendMapper;
+
 import com.metacube.hibernate1.util.DBQueries;
 
 @Repository
@@ -28,9 +23,6 @@ public class EmployeeDao {
 	
 	@Autowired
 	private SessionFactory sessionFactory;
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
 	
 	public void insertEmployee(Employee employee) {
 		Session session = sessionFactory.getCurrentSession();
@@ -43,12 +35,11 @@ public class EmployeeDao {
 		return empList;
 	}
 	
+	//code updated
 	public List<Employee> fetchFriend(int id, String org) {
-		System.out.println("org fetch = "+ org);
-		RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<Employee>(Employee.class);
-		List<Employee> empList = (List<Employee>) jdbcTemplate.query(DBQueries.GETFRIEND, new FriendMapper(), id, org);
-		System.out.println("ORGAN  =  "+empList.get(1).getOrg());
-		return empList;
+		Session session = sessionFactory.getCurrentSession();
+		String query = "from Employee where id <> " + id +" and organization='"+ org + "'";
+		return session.createQuery(query, Employee.class).getResultList();
 	}
 	
 	public Employee updateEmployee(Employee employee) {
@@ -61,18 +52,23 @@ public class EmployeeDao {
 	}
 	
 	public int getLastInsertedId() {
-		int emp =  jdbcTemplate.queryForObject(DBQueries.GETLASTEMPLOYEE, Integer.class);
-		return emp;
+		Session session = sessionFactory.getCurrentSession();
+		return session.createQuery("select max(id) from Employee", Integer.class).getSingleResult();
 	}
 	
 	public Employee getEmployeeToEdit(int id) {
-		RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<Employee>(Employee.class);
-		Employee emp = jdbcTemplate.queryForObject(DBQueries.GETBYID, new FriendMapper(), id);
-		return emp;
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(Employee.class, id);
 	}
 	
 	public Employee getEmployeeById(int id) {
-		RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<Employee>(Employee.class);
-		return jdbcTemplate.queryForObject(DBQueries.GETBYID, new FriendMapper(), id);
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(Employee.class, id);
 	}
+	
+	public Employee getEmployeeByEmail(String email) {
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(Employee.class, email);
+	}
+	
 }
